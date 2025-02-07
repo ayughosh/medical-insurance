3b87d7bea495f895e46e1c5ad50b6b9b49f67984
Ayushi Ghosh <ayushighosh20@gmail.com>
2025-02-07 11:42:07 +0530

    Updating code back to original

--- medical_insurance.py
+++ medical_insurance.py
@@ -7,7 +7,7 @@ smoker_statuses = []
 regions = []
 insurance_charges = []
 def update_list (lst, column_name):
-    with open('C:\python-portfolio-project-starter-files\insurance.csv',mode='r') as insurance_csv: 
+    with open('insurance.csv',mode='r') as insurance_csv: 
         csv_reader = csv.DictReader(insurance_csv)
         for row in csv_reader:
             lst.append(row[column_name])
@@ -91,69 +91,66 @@ if __name__ == '__main__':
     counter=0
     b=0
     
-    
-    
-    
-patient_info = PatientsInfo(ages,sexes,bmis,num_children,smoker_statuses,regions,insurance_charges)
-patient_info.analyze_ages()
-patient_info.average_charges()
-patient_info.analyze_sex()
-patient_info.analyze_smoker()
+    patient_info = PatientsInfo(ages,sexes,bmis,num_children,smoker_statuses,regions,insurance_charges)
+    patient_info.analyze_ages()
+    patient_info.average_charges()
+    patient_info.analyze_sex()
+    patient_info.analyze_smoker()
 
-zipped_charges=list(zip(ages,insurance_charges))
-cost_by_age=[[int(age), float(charge)] for age, charge in zipped_charges]
-categories = ['-25 years', '26-40 years', '41-60 years', '+60 years']
-patients_per_category = [i * 0 for i in range(len(categories))]
-cost_per_category = [i * 0 for i in range(len(categories))]
-for index in range(len(cost_by_age)):
-    
-    if cost_by_age[index][0] <= 25:
-        patients_per_category[0] += 1
-        cost_per_category[0] += cost_by_age[index][1]
+    zipped_charges=list(zip(ages,insurance_charges))
+    cost_by_age=[[int(age), float(charge)] for age, charge in zipped_charges]
+    categories = ['-25 years', '26-40 years', '41-60 years', '+60 years']
+    patients_per_category = [i * 0 for i in range(len(categories))]
+    cost_per_category = [i * 0 for i in range(len(categories))]
+    for index in range(len(cost_by_age)):
         
-    elif cost_by_age[index][0] >= 26 and cost_by_age[index][0] <= 40:
-        patients_per_category[1] += 1
-        cost_per_category[1] += cost_by_age[index][1]
-        
-    elif cost_by_age[index][0] >= 41 and cost_by_age[index][0] <= 60:
-        patients_per_category[2] += 1
-        cost_per_category[2] += cost_by_age[index][1]
-        
-    else:
-        patients_per_category[-1] += 1
-        cost_per_category[-1] += cost_by_age[index][1]
+        if cost_by_age[index][0] <= 25:
+            patients_per_category[0] += 1
+            cost_per_category[0] += cost_by_age[index][1]
+            
+        elif cost_by_age[index][0] >= 26 and cost_by_age[index][0] <= 40:
+            patients_per_category[1] += 1
+            cost_per_category[1] += cost_by_age[index][1]
+            
+        elif cost_by_age[index][0] >= 41 and cost_by_age[index][0] <= 60:
+            patients_per_category[2] += 1
+            cost_per_category[2] += cost_by_age[index][1]
+            
+        else:
+            patients_per_category[-1] += 1
+            cost_per_category[-1] += cost_by_age[index][1]
 
-avg_cost_category = []
+    avg_cost_category = []
 
-for index in range(len(categories)):
-    average_cost = round((cost_per_category[index] / patients_per_category[index]),2)
-    avg_cost_category.append([categories[index], average_cost])
-    print('The average insurance cost for people in category "{c}" is: {d} dollaras'. format(
-        c = avg_cost_category[index][0], d = avg_cost_category[index][1]))
+    for index in range(len(categories)):
+        average_cost = round((cost_per_category[index] / patients_per_category[index]),2)
+        avg_cost_category.append([categories[index], average_cost])
+        print('The average insurance cost for people in category "{c}" is: {d} dollaras'. format(
+            c = avg_cost_category[index][0], d = avg_cost_category[index][1]))
 
-total_sum_for_men=0
-total_sum_for_women=0
-category_to_num = {'male': 0, 'female': 1}
-sexes_new = [category_to_num[category] for category in sexes]
-cost_by_sex = [[sex, float(charge)] for sex, charge in list(zip(sexes_new,insurance_charges))]
-c=0
-b=0
-for i in range(len(cost_by_sex)):
-    for j in range(len(cost_by_sex[i])):
-        if cost_by_sex[i][j]==0:
-            c=c+1
-            total_sum_for_men=total_sum_for_men+cost_by_sex[i][j+1]
-        elif cost_by_sex[i][j]==1:
-            b=b+1
-            total_sum_for_women=total_sum_for_women+cost_by_sex[i][j+1]
-print("Total charge for men" + str(total_sum_for_men))
-print("Total charge for women" + str(total_sum_for_women))
-avarage_cost_for_women=total_sum_for_women/b
-average_cost_for_men=total_sum_for_men/c
-print("Average cost for Men " + str(average_cost_for_men))
-print("Average cost for Women" + str(avarage_cost_for_women))
+    total_sum_for_men=0
+    total_sum_for_women=0
+    category_to_num = {'male': 0, 'female': 1}
+    sexes_new = [category_to_num[category] for category in sexes]
+    cost_by_sex = [[sex, float(charge)] for sex, charge in list(zip(sexes_new,insurance_charges))]
+    c=0
+    b=0
+    for i in range(len(cost_by_sex)):
+        for j in range(len(cost_by_sex[i])):
+            if cost_by_sex[i][j]==0:
+                c=c+1
+                total_sum_for_men=total_sum_for_men+cost_by_sex[i][j+1]
+            elif cost_by_sex[i][j]==1:
+                b=b+1
+                total_sum_for_women=total_sum_for_women+cost_by_sex[i][j+1]
+    print("Total charge for men" + str(total_sum_for_men))
+    print("Total charge for women" + str(total_sum_for_women))
+    avarage_cost_for_women=total_sum_for_women/b
+    average_cost_for_men=total_sum_for_men/c
+    print("Average cost for Men " + str(average_cost_for_men))
+    print("Average cost for Women" + str(avarage_cost_for_women))
 
-patients = patient_info.create_dictionary()
+    patients = patient_info.create_dictionary()
 
     
 
